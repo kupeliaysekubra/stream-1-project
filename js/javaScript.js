147acb8150db35bee58e2742b95362f9c21f9a21
Ayse Kubra <aysekubrakupeli@gmail.com>
2017-10-04 15:25:27 +0000

    adding files javaScript and CSS files

---
+++ js/javaScript.js
@@ -0,0 +1,12 @@
+<script>
+var ypos,image;
+
+function parallax() {
+        ypos = window.pageYOffset;
+        image = document.getElementById('image');
+        image.style.top = ypos * .4 + 'px';
+}
+
+window.addEventListener('scroll', parallax);
+      
+</script>
